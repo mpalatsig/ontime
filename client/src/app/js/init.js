8fabc4c997ba5be0fde945cf978000f87edc48fe
Marc Palatsí <mpalatsi@me.com>
2017-08-11 04:01:59 +0200

    Layout internal complete

--- client/src/app/js/init.js
+++ client/src/app/js/init.js
@@ -1,9 +1,28 @@
-(function($){
-  $(function(){
 
 
-  $(".dropdown-button").dropdown();
-  $('.button-collapse').sideNav();
+(function($) {
+  $(function() {
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    $('select').material_select();
+    $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown();
+    $('.datepicker').pickadate({
+      hover: false
+    });
+    // $('.dropdown-button').dropdown('open');
+    // $('.dropdown-button').dropdown('close');
+    // $('.dropdown-button').dropdown({
+    //   inDuration: 300,
+    //   outDuration: 225,
+    //   constrainWidth: false, // Does not change width of dropdown to that of the activator
+    //   hover: true, // Activate on hover
+    //   gutter: 0, // Spacing from edge
+    //   belowOrigin: false, // Displays dropdown below the button
+    //   alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    //   stopPropagation: false // Stops event propagation
+    // });
+
+
+  });
+
+})(jQuery);
